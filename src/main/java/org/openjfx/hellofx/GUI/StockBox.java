cb2079d093315d8be931833856e17bd6159fd06d
Noam Greenshtain <noamgrinch@gmail.com>
2019-06-08 15:53:37 +0300

    Working on GUI. synchronizing StockBoxes to look fine

--- src/main/java/org/openjfx/hellofx/GUI/StockBox.java
+++ src/main/java/org/openjfx/hellofx/GUI/StockBox.java
@@ -6,12 +6,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -22,6 +24,7 @@ public class StockBox extends GridPane{
 	
 	private Stock stock;
 	private Label slabel,sname,sprice,sper;
+	private String labeltextcolor = "-fx-text-fill: white";
 	
 	
 	public StockBox(Stock stock) {
@@ -31,18 +34,22 @@ public class StockBox extends GridPane{
 		slabel.setPadding(new Insets(2,15,2,1));
 		slabel.setFont(new Font("Arial",15));
 		slabel.setTextAlignment(TextAlignment.LEFT);
+		slabel.setStyle(labeltextcolor);
 		//slabel.setBorder(new Border(new BorderStroke(Color.BLUE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		sname = new Label(stock.getName());
 		sname.setPadding(new Insets(2,15,2,1));
 		sname.setTextAlignment(TextAlignment.LEFT);
+		sname.setStyle(labeltextcolor);
 		//sname.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		sprice = new Label(Double.toString(stock.getPrice()) + "$");
 		sprice.setPadding(new Insets(2,1,2,15));
 		sprice.setFont(new Font("Arial",15));
 		sprice.setTextAlignment(TextAlignment.RIGHT);
+		sprice.setStyle(labeltextcolor);
 		//sprice.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		sper = new Label(Double.toString(stock.getChangepercent()) + "%");
 		sper.setFont(new Font("Arial",15));
+		//sper.setStyle(labeltextcolor);
 		Color col;
 		if(stock.getChangepercent()>0) {
 			//sper.setStyle("-fx-background-color:linear-gradient(to right top, #48e511,#68e909,#81ee03,#97f201,#acf605);-fx-border-syle: round;-fx-text-alignment: center");
@@ -54,8 +61,9 @@ public class StockBox extends GridPane{
 		}
 		CornerRadii corn = new CornerRadii(5);
 		Background background = new Background(new BackgroundFill(col, corn, Insets.EMPTY));
+		sper.setBackground(background);
 		sper.setPadding(new Insets(2,1,2,15));
-		//sper.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		//sper.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		GridPane.setConstraints(slabel,0,0);
 		GridPane.setConstraints(sname,0,1);
 		GridPane.setConstraints(sprice,1,0);
@@ -64,8 +72,13 @@ public class StockBox extends GridPane{
 		GridPane.setHalignment(sprice, HPos.RIGHT);
 		GridPane.setHalignment(sname, HPos.LEFT);
 		GridPane.setHalignment(slabel, HPos.LEFT);
+		ColumnConstraints column1 = new ColumnConstraints();
+		column1.setPercentWidth(70);
+		this.getColumnConstraints().add(column1);
 		this.getChildren().addAll(slabel,sname,sprice,sper);
+		background = new Background(new BackgroundFill(Color.BLACK, corn, Insets.EMPTY));
 		this.setBackground(background);
+		//this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 
 		
 	}
--- src/main/java/org/openjfx/hellofx/MainFrame.java
+++ src/main/java/org/openjfx/hellofx/MainFrame.java
@@ -14,10 +14,16 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -26,14 +32,19 @@ public class MainFrame extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		
-		stage.setTitle("Stox");
+		stage.setTitle("Stox");	
+		Stock[] ss = new Stock[4];
 
 		try {
 				//LoginFrame.display(); // dispay later on
 				
-				Stock st = Stock.findStockName("BABA");
+				ss[0] = Stock.findStockName("BABA");
+				ss[1] = Stock.findStockName("AWK");
+				ss[2] = Stock.findStockName("AAPL");
+				ss[3] = Stock.findStockName("NVDA");
 				
 				VBox main = new VBox();
+				main.setPadding(new Insets(5,5,5,5));
 			
 				//Edit button
 				BorderPane br = new BorderPane();
@@ -43,13 +54,29 @@ public class MainFrame extends Application{
 				bottom.getChildren().add(edit);
 				bottom.setAlignment(Pos.CENTER);
 				
-				//stocks
-				GridPane lay1 = new GridPane(); 
+				GridPane lay1 = new GridPane();
+				lay1.setVgap(2);
 				lay1.setAlignment(Pos.TOP_CENTER);
-				StockBox sb = new StockBox(st);
-				lay1.getChildren().addAll(sb);
-				GridPane.setFillWidth(sb, true);
-				GridPane.setConstraints(sb, 0, 0);
+				ColumnConstraints column1 = new ColumnConstraints();
+				column1.setPercentWidth(100);
+				lay1.getColumnConstraints().add(column1);
+				StockBox sb;
+				for(int i=0;i<ss.length;i++) {
+					sb = new StockBox(ss[i]);
+					lay1.getChildren().add(sb);
+					GridPane.setFillWidth(sb, true);
+					GridPane.setConstraints(sb, 0, i);			
+				}
+				
+				//stocks
+ 
+				
+				/*CornerRadii corn = new CornerRadii(5);
+				Color col = Color.web("#000000");
+				Background background = new Background(new BackgroundFill(col, corn, Insets.EMPTY));
+				lay1.setBackground(background);*/
+				
+
 				
 				//general configuration
 				main.getChildren().add(lay1);
